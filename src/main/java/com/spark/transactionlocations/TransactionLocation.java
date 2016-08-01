338f5c3ba0e309ddf4f7329041a2b4ee89cd416c
raghunandan-gupta <raghunandan.gupta@exclusively.com>
2016-08-01 12:45:18 +0530

    unused improts removed.

--- src/main/java/com/spark/transactionlocations/TransactionLocation.java
+++ src/main/java/com/spark/transactionlocations/TransactionLocation.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.PairFunction;
 
@@ -21,7 +20,7 @@ public class TransactionLocation {
 
 		try (JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf)) {
 			final String firstLine = javaSparkContext.textFile("/tmp/user").first();
-			
+
 			JavaPairRDD<Integer, String> customerPairs = javaSparkContext.textFile("/tmp/user").filter((x) -> !x.equals(firstLine))
 					.map((x) -> x.split(",")).mapToPair((x) -> new Tuple2<Integer, String>(Integer.parseInt(x[0]), x[3]));
 
@@ -29,14 +28,16 @@ public class TransactionLocation {
 			JavaPairRDD<Integer, Integer> transactionPairs = javaSparkContext.textFile("/tmp/transactions").filter((x) -> !x.equals(firstLineT))
 					.map((x) -> x.split(",")).mapToPair((x) -> new Tuple2<Integer, Integer>(Integer.parseInt(x[2]), Integer.parseInt(x[1])));
 
-			
-			Map<Integer,Object> map = transactionPairs.leftOuterJoin(customerPairs).values().distinct().mapToPair(new PairFunction<Tuple2<Integer,Optional<String>>, Integer, String	>() {
-				@Override
-				public Tuple2<Integer, String> call(Tuple2<Integer, Optional<String>> t) throws Exception {
-					return new Tuple2<Integer, String>(t._1, t._2.get());
-				}
-			}).countByKey();
-			
+			Map<Integer, Object> map = transactionPairs.leftOuterJoin(customerPairs).values().distinct()
+					.mapToPair(new PairFunction<Tuple2<Integer, Optional<String>>, Integer, String>() {
+						private static final long serialVersionUID = 1L;
+
+						@Override
+						public Tuple2<Integer, String> call(Tuple2<Integer, Optional<String>> t) throws Exception {
+							return new Tuple2<Integer, String>(t._1, t._2.get());
+						}
+					}).countByKey();
+
 			System.out.println(map);
 		}
 	}
